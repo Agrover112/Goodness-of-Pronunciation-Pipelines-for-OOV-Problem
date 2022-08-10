006952e755fa8ea93370463b9500130e558710bb
Agrover112 <42321810+Agrover112@users.noreply.github.com>
2022-08-10 21:09:28 +0530

    fix: Fixed and updated with usage

--- collect_transcripts.sh
+++ collect_transcripts.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 
 # Find all the transcripts present in the nested-folders and put them in one text-file
-#
+# Usage:
+# bash collect_transcripts.sh ./train-clean-360/ transcript
+# Output: other.txt
 dir=$1
 file=$2
 cat $(find $dir -name "*.trans.txt") | cut -d' ' -f2- >${file}.txt
