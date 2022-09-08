d1b072b1129909a33964b390b4d421e223f202c7
Agrover112 <42321810+Agrover112@users.noreply.github.com>
2022-09-08 12:25:48 +0530

    Update runAllFiles.sh

--- runAllFiles.sh
+++ runAllFiles.sh
@@ -3,18 +3,22 @@
  set -e
 wavDir=$1;
 transFile=$2 #update this file with your trascriptions file
-outFile=$3; 
+outFile=$3;
 outDir=$4; # update this variable with your destination folder
 currPWD=$PWD
 mkdir -p $outDir
- 
+
 export DIR="/home/chiranjeevi/kaldi";
 export DIR1b=$PWD"/wavlib/exp";
 #export DIR1b=$PWD/"exp"
 export DIR3b=$PWD"/wavlib/data"
 #export DIR3b=$PWD"/data"
-#export lang=$DIR3b"/lang"
-export lang=$PWD"/wavlib/data/langer"
+#export lang=$DIR3b"/lang"              MAKE SURE TO REPLACE lang_* with langer before starting any experiment or reruns.
+#export lang=$PWD"/wavlib/data/langer"  # OOV present
+export lang=$PWD"/wavlib/data/lang_nt" # Offline mode
+#export lang=$PWD"/wavlib/data/lang_onlin" # Online mode
+#export lang=$PWD"/wavlib/data/lang_hybrid" # Hybrid mode
+
 export mfccdir=mfcc
 
 export online_ivector_dir=$DIR1b"/nnet2_online/ivectors_train";
@@ -28,7 +32,7 @@ do
     name=$(basename "$file" .flac)
     directory=$(dirname "$file")
     echo ffmpeg -i "$file" "$directory"/"$name".wav
-    #ffmpeg -i $file $dir/$name.wav 
+    #ffmpeg -i $file $dir/$name.wav
 done
 
 # Changing transcript
@@ -37,47 +41,67 @@ cat $transFile | awk '{gsub("([0-9]+-[0-9]+-[0-9]+)","UTTERENCE_ID",$1)}1' | cat
 
 
 # A counter variable for selecting line from transcript
-counter=1
+export counter=1
 
-for i in $(find "${wavDir}/" -iname "*.wav" -o -iname "*.flac" | sort -n); 
+for i in $(find "${wavDir}/" -iname "*.wav" -o -iname "*.flac" | sort -n);
 do
-	temp=`echo $i | rev | cut -d"." -f2 | cut -d"/" -f1 | rev`;
-	printf "Filename is %s wav \n" "$temp"
-	export temp
-	touch logs/$temp.log
+        temp=`echo $i | rev | cut -d"." -f2 | cut -d"/" -f1 | rev`;
+        printf "Filename is %s wav \n" "$temp"
+        export temp
+        touch logs/$temp.log
 
-	#echo $temp
-	sed -n ${counter}p tempfile > $outFile
-	cat $outFile
-	
-	if [ $counter -eq 1 ];
-	then
-		((++counter));
-		continue;
-	fi
-	printf "Generating alignments for %s \n" "$temp"
-	bash $PWD/run1File.sh $i $outDir/$temp.txt #>> logs/$temp.log 
-	
-	printf "Generating posteriors for %s \n" "$temp"
-	./run1File_posterior.sh $i $currPWD/posterior/${temp}_posterior_infile.ark $wavDir $transFile $outFile 
+        #echo $temp
+        sed -n ${counter}p tempfile > $outFile
+        cat $outFile
+        if [ $counter -eq 2 ];
+        then
+                break;
+        #       ((++counter));
+        #       continue;
+        fi
 
-	printf "Calculating Goodness of Pronounciation scores for %s \n" "$temp"
-	mkdir -p gop
-	cd Goodness-of-Pronounication/
+        ## Uncomment for online mode
+        #bash online_computation.sh $outFile
 
-	python3 prop_gop_eqn.py $currPWD/wavlib/lab/posteriors/${temp}_posterior_infile.ark $currPWD/wavlib/lab/${temp}_alignment_infile.txt $currPWD/gop/${temp}_gop.txt
-	cd ..
-	((++counter))
+        printf "Generating alignments for %s \n" "$temp"
+         bash $PWD/run1File.sh $i $outDir/$temp.txt #>> logs/$temp.log
+
+        printf "Generating posteriors for %s \n" "$temp"
+        ./run1File_posterior.sh $i $currPWD/posterior/${temp}_posterior_infile.ark $wavDir $transFile $outFile
+
+        $DIR/src/bin/post-to-tacc --binary=false $dir/final.mdl "ark:$DIR/src/bin/ali-to-post ark:$dir/ali/${temp}_ali ark:- |" "${temp}.tacc"
+
+        $DIR/src/bin/prob-to-post ark:$currPWD/wavlib/lab/posteriors/${temp}_posterior_infile.ark ark:- |
+                $DIR/src/bin/post-to-pdf-post $dir/final.mdl ark:- ark:- |
+               $DIR/src/bin/post-to-phone-post --transition-id-counts="${temp}.tacc" $dir/final.mdl ark:-  "ark,t:$currPWD/wavlib/lab/posteriors/${temp}_phone_posteriors.ark"
+        rm -rf $dir/ali/*
+        #rm -f ${temp}.tacc
+
+
+        printf "Calculating Goodness of Pronounciation scores for %s \n" "$temp"
+        mkdir -p gop
+        cd Goodness-of-Pronounication/
+
+        python3 prop_gop_eqn.py $currPWD/wavlib/lab/posteriors/${temp}_posterior_infile.ark $currPWD/wavlib/lab/${temp}_alignment_infile.txt $currPWD/gop/${temp}_gop.txt
+
+        #rm -f trans_vocab.txt W
+        #rm -f file_lexicon.txt W
+        #rm -f $libri_path/not_both1.txt
+        #rm -rf wavlib/data/local/dict/online_dict/* W
+        #rm -rf wavlib/data/lang_onlin/*
+        #sudo rm -f $dir/phones.txt
+
+        cd ..
+        ((++counter))
 done
 
 
 
 
-
+echo "DONE"
 
 
 #echo "UTTERENCE_ID" `cat $transFile | grep "\b"$temp" " | cut -d" " -f2- |sed "s/ (/=/g" | cut -d"=" -f1 | sed "s/) /=/g" | cut -d"=" -f2- | sed "s/'/XXXXX/g" | tr '[:punct:]' ' ' | tr -s " " | sed "s/XXXXX/'/g" | sed "s/ '/ /g" | tr '[A-Z]' '[a-z]'` > $outFile
 #cat $outFile
 #awk '{gsub("([0-9]+-[0-9]+-[0-9]+)","UTTERANCE_ID",$1)}1' $transFile | tr '[A-Z]' '[a-z]' | cat >$outFile
 #cat $transFile | tr [A-Z] [a-z]  | awk '{gsub("([0-9]+-[0-9]+-[0-9]+)","UTTERENCE_ID",$1)}1' | cat>$outFile
-
